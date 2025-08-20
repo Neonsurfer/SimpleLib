1411acb5a10c2c27385841d6f4c3ed1f77ff9529
Audiosurfer <vassauder@gmail.com>
2025-08-20 09:31:52 +0200

    initial commit

---
+++ src/main/java/com/simple/simpleLib/SimpleLibApplication.java
@@ -0,0 +1,13 @@
+package com.simple.simpleLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLibApplication.class, args);
+	}
+
+}
